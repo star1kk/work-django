d3bfbb700935f43f89fcadfe3b7b315df9ed099c
Aleksey <alekseynikitin632@gmail.com>
2022-11-25 10:28:53 +0500

    yesss

--- star/users/forms.py
+++ star/users/forms.py
@@ -1,6 +1,9 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+
+from django import forms
+from .models import Image
+
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -13,3 +16,8 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email',)
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ('title', 'image')
--- star/users/models.py
+++ star/users/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(default=0)
+
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.title
