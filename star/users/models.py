7e2d366be0bdc02578e58a6cce19282e8cb108f1
star1kk <is109-starkov@urtt.ru>
2022-12-06 22:24:30 +0500

    strani4ki

--- star/users/models.py
+++ star/users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(default=0)
 
+
 class Image(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images')
